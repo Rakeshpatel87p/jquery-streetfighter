84f8ef61c2cf97e99d255acd8d69fbded6d1ecd0
Rakesh Patel <emailme@rakeshrpatel.com>
2016-03-18 07:53:05 +0530

    finished project

---
+++ js/app.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    $(".ryu").mouseenter(function() {
+        $(".ryu-still").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-ready").show();
+    })
+
+    .mouseleave(function() {
+        $(".ryu-ready").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-still").show();
+
+    })
+
+    .mousedown(function() {
+        playHadouken();
+        $(".ryu-ready").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-still").hide();
+        $(".ryu-throwing").show();
+        $(".hadouken").finish().show().animate({ 'left': '1020px' },
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+
+    .mouseup(function() {
+        $(".ryu-throwing").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-ready").show();
+    })
+
+});
+
+$(document).keydown(function(x) {
+    if (x.keyCode == '88') {
+        $(".ryu-still").hide();
+        $(".ryu-throwing").hide();
+        $(".ryu-ready").hide();
+        $(".ryu-cool").show();
+        //return;
+    }
+})
+
+$(document).keyup(function(xrelease) {
+    if (xrelease.keyCode == '88') {
+        $(".ryu-cool").hide();
+        $(".ryu-throwing").hide();
+        $(".ryu-ready").hide();
+        $(".ryu-still").show();
+    }
+    //Need to get Ryu throwing when mousedown.	
+    //if { run .mousedownfunction()).run()};	
+})
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+
+}
